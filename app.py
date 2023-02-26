7236e165ea9db1a4bb38c81b63b23b415c2829f4
vinaykum70 <122093835+vinaykum70@users.noreply.github.com>
2023-02-26 20:45:37 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,3 +1,23 @@
 import streamlit as st
 
-st.header("This is my first streamlit project")
+
+
+st.title('Welcome !')
+
+st.header('Data Science Internship 2023 !')
+
+st.header('This is my first streamlit project')
+
+
+
+st.image('AI.jpg')
+
+
+st.button('Click Me !')
+
+
+st.balloons()
+st.snow()
+
+with st.sidebar:
+    st.write('Dashboard')
